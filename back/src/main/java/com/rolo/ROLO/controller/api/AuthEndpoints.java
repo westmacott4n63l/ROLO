b107dfd220912a8b00cbb3b12043702c091fea1c
Keu <keuxd2003@gmail.com>
2025-11-21 16:58:01 -0300

    Swagger implemented && /api/endpoints removed

--- back/src/main/java/com/rolo/ROLO/controller/api/AuthEndpoints.java
+++ back/src/main/java/com/rolo/ROLO/controller/api/AuthEndpoints.java
@@ -1,20 +1,13 @@
 package com.rolo.ROLO.controller.api;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import com.rolo.ROLO.model.LoginRequest;
 import com.rolo.ROLO.model.Session;
@@ -23,9 +16,6 @@ import com.rolo.ROLO.service.AuthService;
 @RestController
 @RequestMapping("/api")
 public class AuthEndpoints {
-
-	@Autowired
-	private RequestMappingHandlerMapping handlerMapping;
 	
 	@Autowired
 	private AuthService authService;
@@ -40,39 +30,5 @@ public class AuthEndpoints {
     	authService.invalidateSession(token);
     	return ResponseEntity.status(HttpStatus.OK).build();
     }
-	
-    @GetMapping("/endpoints")
-    public List<Map<String, Object>> listEndpoints() {
-        Map<String, Map<String, List<String>>> groupedEndpoints = handlerMapping.getHandlerMethods().entrySet().stream()
-            .filter(entry -> entry.getValue().getBeanType().isAnnotationPresent(RestController.class))
-            .filter(entry -> entry.getValue().getBeanType().getPackageName().startsWith("com.rolo"))
-            .collect(Collectors.groupingBy(
-                entry -> classifyEndpoint(entry.getKey().toString()),
-                Collectors.groupingBy(
-                    entry -> entry.getValue().getBeanType().getSimpleName(),
-                    Collectors.mapping(entry -> entry.getKey().toString(), Collectors.toList())
-                )
-            ));
-        
-        List<Map<String, Object>> response = new ArrayList<>();
-
-        groupedEndpoints.forEach((type, endpointsMap) -> {
-            Map<String, Object> typeEntry = Map.of("type", type, "endpoints", endpointsMap.entrySet().stream()
-                .map(entry -> Map.of("classType", entry.getKey(), "endpoints", entry.getValue()))
-                .collect(Collectors.toList()));
-            response.add(typeEntry);
-        });
-
-        return response;
-    }
     
-    private String classifyEndpoint(String endpoint) {
-        if (endpoint.contains("db")) {
-            return "DB Endpoints";
-        } else if (endpoint.contains("api")) {
-            return "API Endpoints";
-        } else {
-            return "Extra Endpoints";
-        }
-    }
 }
